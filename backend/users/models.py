84d934c53790cb1a4150cc8da3e0e09f96e167d0
sivasriraj43 <shivasriraj43@gmail.com>
2024-08-27 18:19:59 +0530

    user created

---
+++ backend/users/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save,pre_save
+from django.dispatch import receiver
+
+# Create your models here.
+class Profile(models.Model):
+    USER_TYPE={
+        ("Seller","Seller"),
+        ("Buyer","Buyer"),
+        ("Deeler","Deeler"),
+    }
+    user= models.OneToOneField(User,on_delete=models.CASCADE)
+    user_type=models.CharField(max_length=20,choices=USER_TYPE, default='Buyer',verbose_name='User Type')
+    img=models.FileField(upload_to='image-path',null=True,blank=True)
+
+    def __str__(self):
+        return f'{self.user.username}\'s Profile'
+
+@receiver(post_save,sender=User)
+def profile_create(sender,created,instance,**kwargs):
+    if created:
+        profile=Profile.objects.create(user=instance)
+        profile.save()
+   
+
